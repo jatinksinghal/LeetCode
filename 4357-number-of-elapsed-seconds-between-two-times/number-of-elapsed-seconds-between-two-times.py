28d36c35e8b0071515ced9d365607d237771067e
Jatin Kumar Singhal <singhaljatin1020@gmail.com>
2026-09-06 19:37:48 +0530

    Time: 3 ms (25.68%) | Memory: 19.3 MB (62.35%) - LeetSync

---
+++ 4357-number-of-elapsed-seconds-between-two-times/number-of-elapsed-seconds-between-two-times.py
@@ -0,0 +1,10 @@
+class Solution:
+    def secondsBetweenTimes(self, startTime: str, endTime: str) -> int:
+        a=startTime.replace(':'," ")
+        a=a.split(" ")
+        b=endTime.split(":")
+        t1,t2=0,0
+        for i in range(len(a)):
+            t1+=int(a[i])*(60**(len(a)-1-i))
+            t2+=int(b[i])*(60**(len(a)-1-i))
+        return(max(t1,t2)-min(t1,t2))
