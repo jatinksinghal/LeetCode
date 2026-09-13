e55256aa3e8746070d0c213eed909af52cb8a49f
Jatin Kumar Singhal <singhaljatin1020@gmail.com>
2026-09-13 23:21:07 +0530

    Time: 42 ms (72.96%) | Memory: 19.2 MB (55.61%) - LeetSync

--- 3553-check-if-two-chessboard-squares-have-the-same-color/check-if-two-chessboard-squares-have-the-same-color.py
+++ 3553-check-if-two-chessboard-squares-have-the-same-color/check-if-two-chessboard-squares-have-the-same-color.py
@@ -3,7 +3,6 @@ class Solution:
         # 96
         a=(ord(coordinate1[0])-96+int(coordinate1[1]))
         b=(ord(coordinate2[0])-96+int(coordinate2[1]))
-        print(a,b)
         if (a%2==0 and b%2==0) or (a%2!=0 and b%2!=0):
             return True
         else:
