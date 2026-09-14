55f88b3cbf53f9ba5f68b8a58f246d8a7de31793
Jatin Kumar Singhal <singhaljatin1020@gmail.com>
2026-09-14 18:12:25 +0530

    Time: 834 ms (19.48%) | Memory: 28.2 MB (5.10%) - LeetSync

--- 1800-concatenation-of-consecutive-binary-numbers/concatenation-of-consecutive-binary-numbers.py
+++ 1800-concatenation-of-consecutive-binary-numbers/concatenation-of-consecutive-binary-numbers.py
@@ -4,5 +4,4 @@ class Solution:
         for i in range(1,n+1):
             a.append(format(i,'b'))
         a="".join(a)
-        e=(int(a,2))
-        return e%(10**9 + 7)
+        return (int(a,2))%(10**9 + 7)
