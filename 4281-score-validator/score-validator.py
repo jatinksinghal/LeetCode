34d36212777279a8a97dd70db3bc54098cd85940
Jatin Kumar Singhal <singhaljatin1020@gmail.com>
2026-09-16 15:20:07 +0530

    Time: 5 ms (15.93%) | Memory: 19.4 MB (82.41%) - LeetSync

---
+++ 4281-score-validator/score-validator.py
@@ -0,0 +1,13 @@
+class Solution:
+    def scoreValidator(self, events: list[str]) -> list[int]:
+        t,w=0,0
+        for i in events:
+            if i in "1234567890":
+                t+=int(i)
+            elif i=="WD" or i=="NB":
+                t+=1
+            elif i=="W":
+                w+=1
+            if w==10:
+                break
+        return [t,w]
