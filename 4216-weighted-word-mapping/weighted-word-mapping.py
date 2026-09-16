38bc99f6b5146cee4a07596e70143c4db24cc584
Jatin Kumar Singhal <singhaljatin1020@gmail.com>
2026-09-16 23:21:37 +0530

    Time: 11 ms (50.42%) | Memory: 19.5 MB (9.33%) - LeetSync

--- 4216-weighted-word-mapping/weighted-word-mapping.py
+++ 4216-weighted-word-mapping/weighted-word-mapping.py
@@ -6,7 +6,6 @@ class Solution:
             for j in i:
                 a=ord(j)-97
                 t+=weights[a]
-            print(t)
             b=t%26
             w.append(chr(96+26-b))
         return "".join(w)
