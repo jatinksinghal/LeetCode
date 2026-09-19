5298b776751a919b72e218f8313834f80cf050b2
Jatin Kumar Singhal <singhaljatin1020@gmail.com>
2026-09-19 12:41:34 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (60.95%) - LeetSync

---
+++ 1954-replace-all-digits-with-characters/replace-all-digits-with-characters.py
@@ -0,0 +1,7 @@
+class Solution:
+    def replaceDigits(self, s: str) -> str:
+        s=list(s)
+        for i in range(1,len(s),2):
+            a=int(s[i])
+            s[i]=chr(ord(s[i-1])+a)
+        return "".join(s)
