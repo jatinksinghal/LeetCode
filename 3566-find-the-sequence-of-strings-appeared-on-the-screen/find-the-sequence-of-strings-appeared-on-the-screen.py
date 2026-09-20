2b5b41c1cd9862ab5f26a829bb864a3ac88eb927
Jatin Kumar Singhal <singhaljatin1020@gmail.com>
2026-09-20 18:31:37 +0530

    Time: 108 ms (6.45%) | Memory: 28.4 MB (32.26%) - LeetSync

--- 3566-find-the-sequence-of-strings-appeared-on-the-screen/find-the-sequence-of-strings-appeared-on-the-screen.py
+++ 3566-find-the-sequence-of-strings-appeared-on-the-screen/find-the-sequence-of-strings-appeared-on-the-screen.py
@@ -5,6 +5,5 @@ class Solution:
             for j in range(97,ord(target[i])+1):
                 w1=list(target[:i])
                 w1.append(chr(j))
-                w1="".join(w1)
-                f.append(w1)
+                f.append("".join(w1))
         return f
