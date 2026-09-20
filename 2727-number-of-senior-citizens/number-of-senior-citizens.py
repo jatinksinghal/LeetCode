0592d1d89836c205bacbe98fa6093330185e5a34
Jatin Kumar Singhal <singhaljatin1020@gmail.com>
2026-09-20 10:43:42 +0530

    Time: 4 ms (7.19%) | Memory: 19.2 MB (89.55%) - LeetSync

--- 2727-number-of-senior-citizens/number-of-senior-citizens.py
+++ 2727-number-of-senior-citizens/number-of-senior-citizens.py
@@ -4,8 +4,6 @@ class Solution:
         for i in details:
             a=i[11:13]
             c=int("".join(a))
-            # for i in a:
-            #     c=c*10+(int(i))
             if c>60:
                 t+=1
         return t
