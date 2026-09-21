9bf8be1bbd8c345107510986f9e8b40c31b53bc1
Jatin Kumar Singhal <singhaljatin1020@gmail.com>
2026-09-21 12:15:33 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (66.30%) - LeetSync

--- 822-unique-morse-code-words/unique-morse-code-words.py
+++ 822-unique-morse-code-words/unique-morse-code-words.py
@@ -6,9 +6,7 @@ class Solution:
             w=[]
             for j in range(len(words[i])):
                 k=ord(words[i][j])
-                print(k-96)
                 w.append(a[k-1-96])
             w="".join(w)
-            print(w)
             b.append(w)
         return len(set(b))
