8129a34202a32fb9424438b133d1cd78a0d08e47
Jatin Kumar Singhal <singhaljatin1020@gmail.com>
2026-09-21 10:55:28 +0530

    Time: 55 ms (21.82%) | Memory: 19.7 MB (19.10%) - LeetSync

--- 2502-sort-the-people/sort-the-people.py
+++ 2502-sort-the-people/sort-the-people.py
@@ -1,8 +1,14 @@
 class Solution:
     def sortPeople(self, names: list[str], heights: list[int]) -> list[str]:
-        a=[]
-        for i in range(len(names)):
-            c=heights.index(max(heights))
-            a.append(names[c])
-            heights[c]=0
-        return a
+        # a=[]
+        # for i in range(len(names)):
+        #     c=heights.index(max(heights))
+        #     a.append(names[c])
+        #     heights[c]=0
+        # return a
+        f=[]
+        a=sorted(heights)
+        for i in a:
+            j=heights.index(i)
+            f.append(names[j])
+        return f[::-1]
