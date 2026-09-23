0407d659583725755d19fdf8177eb59f883e51e3
Jatin Kumar Singhal <singhaljatin1020@gmail.com>
2026-09-23 08:25:59 +0530

    Time: 5 ms (5.89%) | Memory: 18.9 MB (100.00%) - LeetSync

--- 2406-decode-the-message/decode-the-message.py
+++ 2406-decode-the-message/decode-the-message.py
@@ -2,17 +2,14 @@ class Solution:
     def decodeMessage(self, key: str, message: str) -> str:
         key=key.replace(" ","")
         a=[]
-        unenc=[]
         for i in key:
             if i not in a:
                 a.append(i)
-        # message=message.split()
-        w=[]
+        w=""
         for i in message:
             if i!=" ":
                 c=a.index(i)
-                w.append(chr(97+c))
-            # unenc.append("".join(w))
+                w+=(chr(97+c))
             else:
-                w.append(" ")
-        return "".join(w)
+                w+=" "
+        return w
